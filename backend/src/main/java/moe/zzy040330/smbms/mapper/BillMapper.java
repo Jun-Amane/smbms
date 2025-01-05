0b54b0939a702811b4d36cdf4f872fadefe7e53b
Jun Amane <Jun.Amane@zzy040330.moe>
2025-01-05 22:14:28 +0800

    feat(bill mapper): add bill mapper xml

--- backend/src/main/java/moe/zzy040330/smbms/mapper/BillMapper.java
+++ backend/src/main/java/moe/zzy040330/smbms/mapper/BillMapper.java
@@ -1,8 +1,12 @@
 package moe.zzy040330.smbms.mapper;
+
 import moe.zzy040330.smbms.entity.Bill;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.security.core.parameters.P;
+
 import java.util.List;
+
 /**
  * BillMapper provides specific data access methods for the Bill entity, including advanced
  * querying and counting bills related to a specific provider.
@@ -15,12 +19,18 @@ public interface BillMapper extends GenericMapper<Bill, Long> {
      * @param providerId the ID of the provider
      * @return the number of bills associated with the given provider
      */
-    int findBillCountByProviderId(@Param("providerId") Long providerId);
+    Integer findBillCountByProviderId(@Param("providerId") Long providerId);
+
     /**
      * Finds bills based on specified query conditions.
      *
-     * @param condition the query conditions packed into a Bill object (supports fuzzy search)
      * @return a list of bills matching the query conditions
      */
-    List<Bill> findAllBillsByQuery(@Param("condition") Bill condition);
+    List<Bill> findAllBillsByQuery(@Param("code") String code,
+                                   @Param("productName") String productName,
+                                   @Param("productDesc") String productDesc,
+                                   @Param("providerCode") String providerCode,
+                                   @Param("providerName") String providerName,
+                                   @Param("isPaid") Integer isPaid
+    );
 }
