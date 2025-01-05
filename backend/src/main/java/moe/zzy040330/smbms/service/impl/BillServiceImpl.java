0b16216ba1b9fca54dbd1cd2059be6afd0779780
a8617667122022 <a8617667122022@163.com>
2025-01-05 16:33:42 +0800

    feat(bill):add Bill ServiceImpl

--- backend/src/main/java/moe/zzy040330/smbms/service/impl/BillServiceImpl.java
+++ backend/src/main/java/moe/zzy040330/smbms/service/impl/BillServiceImpl.java
@@ -1,20 +1,24 @@
 package moe.zzy040330.smbms.service.impl;
+import moe.zzy040330.smbms.entity.Bill;
 import moe.zzy040330.smbms.entity.Provider;
+import moe.zzy040330.smbms.mapper.BillMapper;
 import moe.zzy040330.smbms.mapper.GenericMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.example.mapper.BillMapper;
-import com.example.model.Bill;
-import com.example.service.BillService;
+
+import moe.zzy040330.smbms.service.BillService;
 import org.springframework.stereotype.Service;
 import java.util.List;
+
 @Service
 public class BillServiceImpl extends GenericCrudServiceImpl<Bill, Long> implements BillService {
     private final BillMapper billMapper;//bill mapper, use constructor injection
-    public BillServiceImpl(GenericMapper<Provider,Long> genericMapper, BillMapper billMapper) {
+
+    public BillServiceImpl(GenericMapper<Bill, Long> genericMapper, BillMapper billMapper) {
         super(genericMapper);
         this.billMapper = billMapper;
     }
+
     /**
      * Get a paginated list of bills based on query conditions.
      *
@@ -26,7 +30,7 @@ public class BillServiceImpl extends GenericCrudServiceImpl<Bill, Long> implemen
     @Override
     public PageInfo<Bill> getBillList(Bill condition, Integer pageNum, Integer pageSize) {
         PageHelper.startPage(pageNum, pageSize);
-        List<Bill> bills = billMapper.findByCondition(condition);
+        List<Bill> bills = billMapper.findAllBillsByQuery(condition);
         return new PageInfo<>(bills);
     }
 }
