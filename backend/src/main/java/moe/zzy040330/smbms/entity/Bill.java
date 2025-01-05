9238a4314ab9d537b0f8597b33f3a12ec3163f05
limingxue <3109853832@qqcom>
2025-01-05 14:00:36 +0800

    feat(bill): add Bill entity

---
+++ backend/src/main/java/moe/zzy040330/smbms/entity/Bill.java
@@ -0,0 +1,152 @@
+package moe.zzy040330.smbms.entity;
+import java.util.Calendar;
+import java.util.Date;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+public class Bill {
+    private Long id;// Unique identifier for the bill.
+    private String code;// Code representing the bill.
+    private String productName;// Name of the product described in the bill.
+    private String productDescription;//Description of the product.
+    private String productUnit;//Unit of measurement for the product.
+    private BigDecimal productCount;//Quantity of the product.
+    private BigDecimal totalPrice;// Total price for the billed products.
+    private Integer isPaid;// Payment status indicator.
+    private Provider provider;// Identifier for the provider of the product.
+    private User createdBy;//User ID of the person who created the bill.
+    private Date creationDate;// Date and time when the bill was created.
+    private User modifiedBy;// User ID of the person who last modified the bill.
+    private Date modificationDate;// Date and time when the bill was last updated.
+    private Long provideId;//The identity of provider
+
+    public Bill() {
+    }
+
+    public Bill(Long id, String code, String productName, String productDescription, String productUnit, BigDecimal productCount, BigDecimal totalPrice, Integer isPaid, Provider provider, User createdBy, Date creationDate, User modifiedBy, Date modificationDate, Long provideId) {
+        this.id = id;
+        this.code = code;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productUnit = productUnit;
+        this.productCount = productCount;
+        this.totalPrice = totalPrice;
+        this.isPaid = isPaid;
+        this.provider = provider;
+        this.createdBy = createdBy;
+        this.creationDate = creationDate;
+        this.modifiedBy = modifiedBy;
+        this.modificationDate = modificationDate;
+        this.provideId = provideId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductUnit() {
+        return productUnit;
+    }
+
+    public void setProductUnit(String productUnit) {
+        this.productUnit = productUnit;
+    }
+
+    public BigDecimal getProductCount() {
+        return productCount;
+    }
+
+    public void setProductCount(BigDecimal productCount) {
+        this.productCount = productCount;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getIsPaid() {
+        return isPaid;
+    }
+
+    public void setIsPaid(Integer isPaid) {
+        this.isPaid = isPaid;
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public User getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(User modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+    public Long getProvideId() {
+        return provideId;
+    }
+
+    public void setProvideId(Long provideId) {
+        this.provideId = provideId;
+    }
+}
